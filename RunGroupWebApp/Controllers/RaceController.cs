f86ab0b964332a79bdae7f0cd43b6356ee87ea16
sinan <aslansinan586@gmail.com>
2022-11-10 10:47:47 +0300

    first controller created for app

---
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroupWebApp.Data.Enum
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
