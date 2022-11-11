867fd88c1785d7080cdd8f2e4f52cc8f0237bbe9
sinan <aslansinan586@gmail.com>
2022-11-11 10:31:57 +0300

    Creating data in wievs and send them to do server

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -29,5 +29,20 @@ namespace RunGroupWebApp.Controllers
 
             return View(club);
         }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Club club)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(club);
+            }
+            _clubRepository.Add(club);
+            return RedirectToAction("Index");
+        }
     }
 }
--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -28,5 +28,19 @@ namespace RunGroupWebApp.Data.Enum
             var race = await _raceRepository.GetByIdAsync(id);
             return View(race);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Race race)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(race);
+            }
+            _raceRepository.Add(race);
+            return RedirectToAction("Index");
+        }
     }
 }
